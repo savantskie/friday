f19b2853d6dac600519911e687f8e6ff1a3951a5
savantskie <nmerrill2000@gmail.com>
2025-09-27 22:55:21 -0500

    switched to linux

--- fridaycaddylaunch.sh
+++ fridaycaddylaunch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Linux version of Friday Caddy Launch
+
 sleep 60
-cd /media/nate/Friday/Friday/caddy
-./caddy run --config Caddyfile --watch
+
+# Navigate to the caddy directory
+cd /media/nate/Friday/Friday/caddy || { echo "Error: Failed to change directory."; exit 1; }
+
+# Run caddy using the global path
+/usr/bin/caddy run --config Caddyfile --watch
